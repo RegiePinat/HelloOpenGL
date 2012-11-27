3e174127e4ddf914c94d9c2791052e128bea8b5b
RegiePinat <regie@klab.com>
2012-11-27 14:00:25 +0800

    first

---
+++ HelloOpenGL/HelloOpenGLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloOpenGLAppDelegate.h
+//  HelloOpenGL
+//
+//  Created by Regie G. Pinat on 11/27/12.
+//  Copyright (c) 2012 Regie G. Pinat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloOpenGLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloOpenGL/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloOpenGL
+//
+//  Created by Regie G. Pinat on 11/27/12.
+//  Copyright (c) 2012 Regie G. Pinat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HelloOpenGLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HelloOpenGLAppDelegate class]));
+    }
+}
